f3bf55d1178153a33617485dc1eeb039cb6546d9
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-03-28 22:36:34 -0500

    cleanup allocations

--- src/exec.zig
+++ src/exec.zig
@@ -15,15 +15,18 @@ pub const Exec = struct {
     pub fn init(tokens: []Token, owned_alloc:std.mem.Allocator) !Exec {
         //var arena = std.heap.ArenaAllocator.init(owned_alloc);//std.heap.page_allocator);
         //const alloc = arena.allocator();
-        return .{
-            .in = tokens,
+        var foo =  Exec{
+            .in = undefined,
             .pos = null,
             .cur = undefined,
             .alloc = owned_alloc,
         };
+        foo.in = try tokenizer.dupe(foo.alloc, tokens);
+        return foo;
     }
 
     pub fn deinit(self:*Exec) void {
+        //tokenizer.free(self.alloc, self.in);
         _ = self;
         //_ = self.arena.reset(.free_all);
         //self.arena.deinit();
@@ -46,7 +49,10 @@ pub const Exec = struct {
             switch (token.type) {
                 .FN => {
                     const argv = try self.get_args();
-                    defer self.alloc.free(argv);
+                    defer {
+                        tokenizer.free(self.alloc, argv); 
+                        self.alloc.free(argv);
+                    }
                     try self.run(token, argv);
                 },
                 else => @panic(try std.fmt.allocPrint(
@@ -59,13 +65,13 @@ pub const Exec = struct {
     fn get_args(self:*Exec) ![]Token {
         var mem = try std.ArrayList(Token).initCapacity(self.alloc, 0);
         defer {
-            for (mem.items) |token| self.alloc.free(token.raw);
+            tokenizer.free(self.alloc, mem.items);
             _ = mem.deinit(self.alloc);
         }
         loop: while (self.peek()) |tok| {
             _ = self.next();
             if (tok.type != .EOX) {
-                try @constCast(&tok).print();
+                //try @constCast(&tok).print();
                 try mem.append(self.alloc, tok);
             } else
                 break :loop;
@@ -76,31 +82,34 @@ pub const Exec = struct {
     fn string_args(self:*Exec, cmd:Token, args:[]Token) ![][]const u8 {
         var argv = try std.ArrayList([]const u8).initCapacity(self.alloc, 0);
         defer {
-            for (argv.items) |arg| self.alloc.free(arg);
+            for (argv.items) |a| self.alloc.free(a);
             _ = argv.deinit(self.alloc);
         }
-        try argv.append(self.alloc, cmd.raw);
-        for (args) |a| switch (a.value_type.?) {
+        try argv.append(self.alloc, try self.alloc.dupe(u8, cmd.raw));
+        for (args) |a| {
+                switch (a.value_type.?) {
 
-            .FLAG => {
-                defer self.alloc.free(a.raw);
-                const converted = if (a.raw.len > 1)
-                    try std.fmt.allocPrint(self.alloc, "--{s}", .{ a.raw })
-                else
-                    try std.fmt.allocPrint(self.alloc, "-{s}", .{ a.raw });
-                try argv.append(self.alloc, converted);
-            },
+                .FLAG => {
+                    const converted = if (a.raw.len > 1)
+                        try std.fmt.allocPrint(self.alloc, "--{s}", .{ a.raw })
+                    else
+                        try std.fmt.allocPrint(self.alloc, "-{s}", .{ a.raw });
+                    try argv.append(self.alloc, converted);
+                },
 
-            else => try argv.append(self.alloc, a.raw),
-        };
+                else => try argv.append(self.alloc, try self.alloc.dupe(u8, a.raw)),
+            }
+        }
         return try argv.toOwnedSlice(self.alloc);
     }
 
     fn run(self:*Exec, cmd:Token, args:[]Token) !void {
         const argv = try self.string_args(cmd, args);
-        defer self.alloc.free(argv);
+        defer for (argv) |a| self.alloc.free(a);
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        defer _ = gpa.deinit();
         var child = std.process.Child{
-            .allocator = self.alloc,
+            .allocator = gpa.allocator(),
             .argv = argv,
             .stdout_behavior = .Inherit,
             .stderr_behavior = .Inherit,
