383c9aad61da946a14bc7b19d9635a0994a71013
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-03 12:09:00 -0500

    helper to get array of keyword duplicates

--- src/globs.zig
+++ src/globs.zig
@@ -1,4 +1,11 @@
 const std = @import("std");
 
+const Keyword = @import("tokenizer.zig").Token.Keyword;
+
 pub const stdout = &@constCast(&std.fs.File.stdout().writer(&.{})).interface;
 pub const stderr = &@constCast(&std.fs.File.stderr().writer(&.{})).interface;
+
+pub const dupe_keywords = struct {
+    pub var @"else" = [_]Keyword{ .@"?!", .@"else" };
+    pub var @"if" = [_]Keyword{ .@"?", .@"if" };
+};
