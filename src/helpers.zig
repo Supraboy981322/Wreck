859f0a3c1ed5c821feab85b527b8d65f68ceb838
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-06 18:47:44 -0500

    helper TokenIterator

--- src/helpers.zig
+++ src/helpers.zig
@@ -1,5 +1,84 @@
 const std = @import("std");
+const globs = @import("globs.zig");
+const Token = @import("types.zig").Token;
 
 pub fn is_num(b:u8) bool {
     return b >= '0' and b <= '9';
 }
+
+pub const TokenIterator = struct {
+    in:[]Token,
+    cur:Token = undefined,
+    pos:?usize = null,
+    opts:Opts,
+    
+    pub const FnHook = *const fn(Token) anyerror!void;
+    
+    pub const Opts = struct {
+        use_void:bool = true,
+        hook_on_next:?FnHook = null,
+    };
+
+    pub fn init(in:[]Token, opts:Opts) TokenIterator {
+        return .{
+            .in = in,
+            .opts = opts,
+        };
+    }
+
+    pub fn can_seek(self:*TokenIterator, direction:enum{ NEXT, BACK }) bool {
+        return
+            for ([_]bool{
+
+                if (direction == .BACK)
+                    self.pos == null
+                else
+                    false,
+
+                if (direction == .BACK)
+                    self.pos.? < 1
+                else
+                    self.pos.? >= self.in.len,
+
+            }) |cannot| {
+                if (cannot) break false;
+            } else
+                true;
+
+    }
+
+    pub fn next(self:*TokenIterator) !?Token {
+
+        self.pos = if (self.pos) |p| p + 1 else 0;
+
+        if (!self.can_seek(.NEXT)) return null;
+
+        self.cur = self.in[self.pos.?];
+        if (self.opts.hook_on_next) |f| try f(self.cur);
+
+        return self.cur;
+    }
+
+    pub fn void_or_null(self:*TokenIterator) ?Token {
+        return if (self.opts.use_void)
+            globs.void_token
+        else
+            null;
+    }
+
+    pub fn back(self:*TokenIterator) ?Token {
+        if (!self.can_seek(.BACK))
+            return self.void_or_null();
+
+        if (self.pos.? > 0) self.pos.? -= 1;
+
+        self.cur = self.in[self.pos.?];
+        return self.cur;
+    }
+
+    pub fn previous(self:*TokenIterator) ?Token {
+        if (!self.can_seek(.PREVIOUS))
+            return self.void_or_null();
+        return self.in[self.pos.? - 1];
+    }
+};
