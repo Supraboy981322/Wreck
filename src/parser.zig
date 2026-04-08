67cb4e0c3c3ea0ac92884066503947553783ca94
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-04-08 11:29:59 -0500

    helper to parsing hex escapes

--- src/parser.zig
+++ src/parser.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 const globs = @import("globs.zig");
+const hlp = @import("helpers.zig");
+
+const print = hlp.print_or_panic;
 
 const stdout = globs.stdout;
+const stderr = globs.stderr;
 const Tokenizer = @import("tokenizer.zig").Tokenizer;
 const Token = @import("tokenizer.zig").Token;
 const Tokenized = @import("types.zig").Tokenized;
@@ -27,3 +31,60 @@ pub fn unescape(alloc:std.mem.Allocator, in:[]u8) ![]u8 {
     });
     return try out.toOwnedSlice(alloc);
 }
+
+pub const strings = struct {
+    fn invalid(
+        opts:struct {
+            direct:?[]u8 = null,
+            line:?[]u8 = null,
+            start:?[]u8 = null,
+            end:?[]u8 = null,
+            expected:?[]const u8 = null,
+            problem:?[]const u8 = null,
+        }
+    ) void {
+        print(.ERR, "invalid string:\n", .{});
+        if (opts.direct) |direct|
+            print(.ERR, "\t{s}\n", .{direct});
+        if (opts.line) |line|
+            print(.ERR, "\t{s}\n", .{line});
+        if (opts.expected) |expected|
+            print(.ERR, "\texpected {s}\n", .{expected});
+        if (opts.problem) |problem|
+            print(.ERR, "\t{s}\n", .{problem});
+        std.process.exit(1);
+    }
+    pub fn hex(pos:*usize, in:[]u8) u8 {
+        var i = pos.*; 
+        if (in[i..].len < 3) strings.invalid(.{
+            .expected = "hex",
+            .problem = "hex not long enough (expected format: \\xXX)"
+        });
+        i += 1;
+        const start = i;
+        var v:u8 = 0;
+        while (i < start + 3) : (i += 1) {
+            v *= 16;
+            v += switch (in[i]) {
+                '0'...'9' => in[i] - '0',
+                'a'...'f' => in[i] - 'a' + 10,
+                'A'...'F' => in[i] - 'A' + 10,
+                else => {
+                    strings.invalid(.{
+                        .expected = "hex",
+                        .problem = b: {
+                            //'allocPrint(...)'? Why would I allocate the string just to add one byte?
+                            const first_half = "invalid character in escape: ";
+                            var buf:[first_half.len + 1]u8 = undefined;
+                            for (first_half, 0..) |b, j| buf[j] = b;
+                            buf[buf.len - 1] = in[i];
+                            break :b &buf;
+                        },
+                    });
+                    unreachable;
+                },
+            };
+        }
+        return v;
+    }
+};
