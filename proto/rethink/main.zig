144b9c66aba8116f6efd6fa0d3b24ac6a5831145
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-30 16:42:47 -0500

    refactor tokenizer for recursion

--- proto/rethink/main.zig
+++ proto/rethink/main.zig
@@ -46,7 +46,11 @@ pub fn main(init:std.process.Init) !void {
     const reader = &file_reader.interface;
 
     var tokenizer:@import("tokenizer.zig").Tokenizer = try .init(alloc);
-    const res = try tokenizer.do(reader);
+    const tokens = try tokenizer.do(reader, null);
+
+    //var finalizer:@import("finalizer.zig").Finalizer = try .init(alloc);
+    //const res = try finalizer.do(tokens);
+    const res = tokens;
 
     var interpreter:@import("interpreter.zig").Interpreter = try .init(init.io, alloc);
     _ = try interpreter.do(res);
--- proto/rethink/tokenizer.zig
+++ proto/rethink/tokenizer.zig
@@ -1,23 +1,25 @@
 const std = @import("std");
 const types = @import("types.zig");
+const hlp = @import("helpers.zig");
 
 const Token = types.Token;
 const Variable = types.Variable;
 const Arg = types.Arg;
 const Block = types.Block;
 
+pub const TokenizerError = error {
+} || std.mem.Allocator.Error;
+
 pub const Tokenizer = struct {
     mem:std.ArrayList(u8) = .empty,
-    res:Block,
     alloc:std.mem.Allocator,
-    reader:*std.Io.Reader,
+    reader:?*std.Io.Reader = null,
     arena:std.heap.ArenaAllocator,
 
     pub fn init(alloc:std.mem.Allocator) !Tokenizer {
         const foo:Tokenizer = .{
             .alloc = alloc,
-            .reader = undefined,
-            .res = Block.init(alloc, null),
+            .reader = null,
             .arena = std.heap.ArenaAllocator.init(alloc),
         };
         return foo;
@@ -28,26 +30,32 @@ pub const Tokenizer = struct {
         _ = self.arena.deinit();
     }
 
-    pub fn recurse(self:*Tokenizer) !Block {
-        var tokenizer:Tokenizer = .init(self.alloc);
-        return try tokenizer.do(self.reader);
+    pub fn recurse(self:*Tokenizer, name:?[]u8) !Block {
+        var tokenizer:Tokenizer = try .init(self.alloc);
+        return try tokenizer.do(self.reader.?, name);
     }
 
-    pub fn do(self:*Tokenizer, reader:*std.Io.Reader) !Block {
+    pub fn do(self:*Tokenizer, reader:*std.Io.Reader, name:?[]u8) TokenizerError!Block {
         // TODO:  helpers to dupe result so arena can be reset
         // defer self.arena.reset(.free_all);
         const alloc = self.arena.allocator();
-        self.reader = reader;
+        if (self.reader == null) self.reader = reader;
 
         var mem:std.ArrayList(u8) = .empty;
         defer mem.deinit(alloc);
 
-        var res:Block = .init(self.alloc, null);
+        var res:Block = .init(self.alloc, name);
 
-        var block:?Block = null;
+        var function:?Block = null;
+        _ = &function; // NOTE: may need this
+
+        var depth_tracker:hlp.DepthTracker(u8) = try .init();
+        _ = &depth_tracker; // NOTE: may need this
 
         var esc:bool = false;
+        var label_name:?[]u8 = null;
         var string:?u8 = null;
+
         while (reader.takeByte() catch null) |b| {
             if (esc) {
                 esc = false;
@@ -58,10 +66,7 @@ pub const Tokenizer = struct {
                 if (b == s) {
                     string = null;
                     const str:Token = .{ .type = .{ .string = try mem.toOwnedSlice(alloc) } };
-                    if (block) |*blk|
-                        try @constCast(blk).code.append(self.alloc, str)
-                    else
-                        try res.code.append(self.alloc, str);
+                    try res.code.append(self.alloc, str);
                 } else
                     try mem.append(alloc, b);
                 continue;
@@ -69,58 +74,46 @@ pub const Tokenizer = struct {
             if (std.ascii.isWhitespace(b) or Token.byte_looks_like_symbol(b)) {
                 if (mem.items.len > 0) {
                     const raw = try mem.toOwnedSlice(alloc);
-                    if (block) |*blk|
-                        try @constCast(blk).code.append(self.alloc, .{ .type =
-                            if (raw[0] == '"' and raw[raw.len-1] == '"') .{
-                                .string = raw[1..raw.len-1],
-                            } else if (raw[0] == '$') .{
-                                .variable = Variable.make(raw[1..]),
-                            } else .{
-                                .ident = raw,
-                            }
-                        })
-                    else
-                        try res.code.append(self.alloc, .{ .type =
-                            if (raw[0] == '"' and raw[raw.len-1] == '"') .{
-                                .string = raw[1..raw.len-1],
-                            } else if (raw[0] == '$') .{
-                                .variable = Variable.make(raw[1..]),
-                            } else .{
-                                .ident = raw,
-                            }
-                        });
+                    try res.code.append(self.alloc, .{ .type =
+                        if (raw[0] == '"' and raw[raw.len-1] == '"') .{
+                            .string = raw[1..raw.len-1],
+                        } else if (raw[0] == '$') .{
+                            .variable = Variable.make(raw[1..]),
+                        } else .{
+                            .ident = raw,
+                        }
+                    });
                 }
 
                 if (Token.byte_looks_like_symbol(b)) {
                     const new:Token = .{ .type = .{
                         .symbol = Token.byte_to_symbol(b) orelse unreachable, }
                     };
-                    if (block) |*blk|
-                        try @constCast(blk).code.append(self.alloc, new)
-                     else
-                        try res.code.append(self.alloc, new);
+                    try res.code.append(self.alloc, new);
                 }
                 continue;
             }
             switch (b) {
                 '"' => string = b,
                 '\\' => esc = true,
-                '{' => {}, // TODO: unlabled block
-                '}' => {
-                    if (block == null)
-                        @panic("closing paren outside of block");
-                    try res.to_namespace(
-                        block.?.name orelse @panic("block name null"),
-                        .{ .type = .{ .label = block.? } }
-                    );
-                    block = null;
+                '{' => {
+                    defer { 
+                        if (label_name) |_| label_name = null;
+                    }
+                    const block = try self.recurse(label_name);
+                    const as_token:Token = .{
+                        .type = .{ .label = block }
+                    };
+                    if (label_name) |label|
+                        try res.to_namespace(label, as_token)
+                    else
+                        try res.code.append(self.alloc, as_token);
                 },
+                '}' => return res,
                 ':' => {
-                    if (mem.items.len < 1)
-                        @panic("invalid label, mem empty");
-                    if (block) |_|
-                        @panic("labeled blocks cannot be nested");
-                    block = .init(self.alloc, try mem.toOwnedSlice(self.alloc));
+                    if (label_name != null)
+                        @panic("missplaced colon");
+                    label_name = try mem.toOwnedSlice(self.alloc);
                 },
                 else => try mem.append(alloc, b),
             }
