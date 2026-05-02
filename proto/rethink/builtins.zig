f6bd6ba689c8b5011b98365e5a30b9f0accafe78
keeper of the interwebs <97927259+Supraboy981322@users.noreply.github.com>
2026-05-02 10:58:06 -0500

    panic on bad token type instead of unreachable

--- proto/rethink/builtins.zig
+++ proto/rethink/builtins.zig
@@ -36,7 +36,7 @@ pub fn print(args:[]Token) !void {
                     else => unreachable,
                 };
             },
-            else => unreachable,
+            else => @panic(@tagName(a.type)),
         }
     }
 }
